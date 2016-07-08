cd9ac4bbdf6c781515548d129be52a93f15c4d1c
Emanuel Zúñiga Infante <emanuzuniga@gmail.com>
2016-07-07 18:55:59 -0600

    0.0.2 DRF Added
    DRF Added

--- CotoCo/CotoCo/urls.py
+++ CotoCo/CotoCo/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
