74120e19a65d4062cd7fc063dea39af44f1e4dd0
Emanuel Zúñiga Infante <emanuzuniga@gmail.com>
2016-08-07 11:01:37 -0600

    0.0.19 another test
     another test

---
+++ CotoCo/CotoCo/wsgy-ubuntu.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for CotoCo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+# add the hellodjango project path into the sys.path
+sys.path.append('/var/www/COTOCO/CotoCo/CotoCo')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/Users/emanuelziga/GITHUB/COTOCO/venvcoto/lib/python2.7/site-packages')
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "CotoCo.settings"
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
