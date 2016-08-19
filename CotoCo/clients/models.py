0e76507c186e36301fa1ec56686276e7c7bd316e
Emanuel Zúñiga Infante <emanuzuniga@gmail.com>
2016-08-18 19:04:30 -0600

    0.2.7 Client model modified
    Client model modified

--- CotoCo/clients/models.py
+++ CotoCo/clients/models.py
@@ -9,11 +9,11 @@ from django.db import models
 class Client(models.Model):
 
     client_name = models.CharField(max_length=255, verbose_name='Nombre')
-    client_phone = models.DecimalField(max_digits=11, decimal_places=0, blank=True, verbose_name='Número de Teléfono')
-    client_id = models.DecimalField(max_digits=20, decimal_places=0, default=0, blank=True,
+    client_phone = models.DecimalField(max_digits=11, decimal_places=0, null=True, blank=True, verbose_name='Número de Teléfono')
+    client_id = models.DecimalField(max_digits=20, decimal_places=0, default=0, blank=True, null=True,
                                     verbose_name='Identificación')
-    client_address = models.CharField(max_length=255, verbose_name='Dirección', blank=True, default='')
-    client_email = models.EmailField(blank=True, verbose_name='Email')
+    client_address = models.CharField(max_length=255, null=True,  verbose_name='Dirección', blank=True, default='')
+    client_email = models.EmailField(null=True, blank=True, verbose_name='Email')
 
     def __unicode__(self):
         return '%s' % self.client_name
